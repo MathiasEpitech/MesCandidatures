286e32a99faaedafe3481847d0d8d4bbcc0563d0
MathiasEpitech <mathias.fernandes@epitech.eu>
2023-10-03 09:57:29 +0200

    mes candidatures

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
-import { Navigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function formatDate(dateString) {
